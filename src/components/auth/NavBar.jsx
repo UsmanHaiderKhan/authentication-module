c87817faa2c68583a85ddee478e2f84b88b9a0ff
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-04-21 10:50:32 +0200

    ♻️ Update : Change the Navbar name by using props

--- src/components/auth/NavBar.jsx
+++ src/components/auth/NavBar.jsx
@@ -1,10 +1,10 @@
 
 
-export const NavBar = () =>{
+export const NavBar = (props) =>{
     return(
         <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/">Navbar</a>
+          <a class="navbar-brand" href="/">{props.heading}</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -31,7 +31,7 @@ export const NavBar = () =>{
             </ul>
             <form class="d-flex" role="search">
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">Search</button>
+              {/* <button class="btn btn-outline-success btn-outline-sm" type="submit">Search</button> */}
             </form>
           </div>
         </div>
