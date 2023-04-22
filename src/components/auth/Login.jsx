83a0eee1814e78d6c032646d9c59d27d5dfb0a68
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-04-22 13:15:08 +0200

    ♻️ update : add placeholder and main wrapper div for styling

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -10,20 +10,22 @@ export const Login = (props)=> {
    }
 
    return(
-    <form onSubmit={handleSubmit}>
-    {/* Email */}
-       <label htmlFor="email">Email</label>
-       <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Please Enter Your Email' name='email' id='email' />
-    {/* Password */}
-        <label htmlFor="password">Password</label>
-        <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} name="password" id="password" />
-    {/* Submit Btn */}
-        <button type='submit' className='btn btn-primary'>Login</button>
-    {/* another Statement */}
-        <p>if you don't have an account <button onClick={()=> props.onFormSwitch('register')} >Register</button> Here! </p>
-   
-    </form>
+    <div className="form-auth-container">
+        <h2>Login</h2>
+        <form className='login-form' onSubmit={handleSubmit}>
+        {/* Email */}
+        <label htmlFor="email">Email</label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Please Enter Your Email' name='email' id='email' />
+        {/* Password */}
+            <label htmlFor="password">Password</label>
+            <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} name="password" id="password" placeholder='*******'/>
+        {/* Submit Btn */}
+            <button type='submit' className='btn btn-primary'>Login</button>
+        {/* another Statement */}
+            <p>if you don't have an account <button onClick={()=> props.onFormSwitch('register')} >Register</button> Here! </p>
     
+        </form>
+    </div>
    )
 
 }
