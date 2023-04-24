e8a3efca2591ad1697ba2e896983b83b66793e56
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-04-24 10:30:52 +0200

    ♻️ Update :  Login page styling

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import NavBar from './NavBar';
 
 export const Login = (props)=> {
    const [email, setEmail] = useState('');
@@ -10,6 +11,7 @@ export const Login = (props)=> {
    }
 
    return(
+
     <div className="form-auth-container">
         <h2>Login</h2>
         <form className='login-form' onSubmit={handleSubmit}>
@@ -20,9 +22,9 @@ export const Login = (props)=> {
             <label htmlFor="password">Password</label>
             <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} name="password" id="password" placeholder='*******'/>
         {/* Submit Btn */}
-            <button type='submit' className='btn btn-primary'>Login</button>
+            <button type='submit' className='btn btn-log'>Login</button>
         {/* another Statement */}
-            <p>if you don't have an account <button onClick={()=> props.onFormSwitch('register')} >Register</button> Here! </p>
+            <p className='mt-4'>if you don't have an account <button className='btn btn-primary btn-sm' onClick={()=> props.onFormSwitch('register')} >Register</button> Here! </p>
     
         </form>
     </div>
